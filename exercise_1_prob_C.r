5df75515008f3ab41f11528b053ff47fb2d51f91
Anders Hoel <anho97@live.no>
2021-02-02 17:22:24 +0100

    Finished the main body of the code for C2 and C3

--- exercise_1_prob_C.r
+++ exercise_1_prob_C.r
@@ -33,3 +33,43 @@ theta_interval = c(theta+10^(-5/2)*qnorm(0.025)*sqrt(var_theta),theta+10^(-5/2)*
 square = function(x){
   return(x^2)
 }
+
+
+################ Problem C 2: ################
+importance.sampling.expnor = function(n,h){
+  xs = sqrt(16-2*log(runif(n))) #Samples the proposal distribution
+  gs = exp(8)*xs*exp(-1/2*xs^2) 
+  fs = (2*pi)^(-1/2)*exp(-1/2*xs^2)
+  ws = fs/gs #computes the weights using the ratio of the probability densities
+  vals = h(xs)
+  est = (1/n)*sum(vals*ws) #computes the estimate
+  var_est = 1/(n-1)*sum((vals*ws-est)^2) #Computes an estimate for the variance of the MC estimate
+  return(c(est,var_est))
+}
+
+theta2_vec = importance.sampling.expnor(100000,indicator.g4)
+theta2 = theta2_vec[1]
+var_theta2 = theta2_vec[2]
+
+theta2_interval = c(theta2+10^(-5/2)*qnorm(0.025)*sqrt(var_theta2),theta2+10^(-5/2)*qnorm(0.975)*sqrt(var_theta2))
+width2 = theta2_interval[2]-theta2_interval[1]
+
+
+################ Problem C 3: ################
+antithetical.importance = function(n,h){
+  us = runif(n)
+  xs = c(sqrt(16-2*log(us)),sqrt(16-2*log(1-us))) #Samples antithetically from the distribution g
+  gs = exp(8)*xs*exp(-1/2*xs^2) 
+  fs = (2*pi)^(-1/2)*exp(-1/2*xs^2)
+  ws = fs/gs #computes the weights using the ratio of the probability densities
+  vals = h(xs)
+  est = (1/(2*n))*sum(vals*ws) #computes the estimate
+  var_est = 1/(2*n-1)*sum((vals*ws-est)^2) #Computes an estimate for the variance of the MC estimate
+  return(c(est,var_est))
+}
+
+theta3_vec = antithetical.importance(50000, indicator.g4)
+theta3 = theta3_vec[1]
+var_theta3 = theta3_vec[2]
+
+theta3_interval = c(theta3+10^(-5/2)*qnorm(0.025)*sqrt(var_theta3),theta3+10^(-5/2)*qnorm(0.975)*sqrt(var_theta3))
