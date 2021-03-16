abf8abb2c9bce9b58a33fd62379e3594dc1a2f20
Anders Hoel <anho97@live.no>
2021-03-16 13:32:40 +0100

    Added code for plotting the gaussian data

--- 2B2.r
+++ 2B2.r
@@ -1,7 +1,30 @@
 library(forecast)
 library(tidyverse)
+library(ggplot2)
+library(latex2exp)
+source("exercise_1_prob_A.r")
 
 yt <- read.table("Gaussiandata.txt") %>% 
-  as.matrix()
+  setNames("y_t") %>% 
+  mutate(t = row_number())
 
-plot(yt)
+# Plotting the time series ------------------------------------------------
+
+ggplot(yt, aes(x=t,y=y_t)) + 
+  geom_point() +
+  ggtitle("Gaussian data") +
+  ylab(TeX("$y_t$")) +
+  theme_bw()
+
+
+# Gibbs sampling ----------------------------------------------------------
+
+# gibbs = function(N){
+#   #Generate a gibbs sampling Markov Chain of length N
+#   T=20
+#   thetas = vector(length = N)
+#   etas = matrix(nrow = N, ncol = T)
+#   
+#   
+# }
+
