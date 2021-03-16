04d41a0cc486bb988d48bae06cf8b07b25ba7e0a
Anders Hoel <anho97@live.no>
2021-03-16 23:48:46 +0100

    Finished Gibbs sampler, and started work on the monte carlo estimate with confidence intervals

--- 2B2.r
+++ 2B2.r
@@ -2,6 +2,7 @@ library(forecast)
 library(tidyverse)
 library(ggplot2)
 library(latex2exp)
+library(coda)
 source("exercise_1_prob_A.r")
 source("exercise_1_prob_B.r")
 
@@ -22,7 +23,7 @@ ggplot(yt, aes(x=t,y=y_t)) +
 
 Q.matrix <- function(theta){
   #Constructs the sparse presision matrix for the gaussian field
-  Q = diag(c(1,5,rep(6,16),5,1))
+  Q = diag(c(1,5,rep(6,16),5,1)*theta)
   diag(Q[-1,]) <- c(-2,rep(-4,17),-2)*theta
   diag(Q[,-1]) <- c(-2,rep(-4,17),-2)*theta
   diag(Q[3:20,]) <- rep(1,18)*theta
@@ -41,8 +42,7 @@ gibbs = function(N){
   
   #initialize theta and eta
   theta = 1
-  etavec = c(1:(T/2),(T/2):1)*100 #Careful about initial conditions
-  print(paste("etavec",etavec))
+  etavec = c(1:(T/2),(T/2):1) #Careful about initial conditions
   
   #Load the gaussian data
   y <- read.table("Gaussiandata.txt") %>% 
@@ -51,18 +51,39 @@ gibbs = function(N){
   for (i in 1:N) {
     #Theta update:
     ratepar = 1+1/2*t(etavec) %*% Q.matrix(1) %*% etavec
-    print(paste("ratepar",ratepar))
+    #sampling from gamma distribution
     theta <- gamma.B.4.sample(1,T/2,ratepar)
     thetas[i] = theta
-    print(paste("Theta",theta))
 
     #Eta update:
     Q <- Q.matrix(theta)
-    print(Q)
     I = diag(20)
+    #sampling from multivariate normal
     etavec = one.multivar.norm.sample(mu = (solve(Q+I)%*%y),Sigma = solve(Q+I))
     etas[i,] = etavec
   }
-  return(thetas)
+  #returning values as a list
+  return(list(thetas=thetas,etas=etas))
 }
+
+mc.estimate.smooth <- function(N){
+  #Function for computing the posterior mean of the marginal etas
+  
+  etas = gibbs(N)$etas #Sampling the etas from our gibbs sampler
+  
+  #Computing column wise MC estimates, keeping half of the simulated chain
+  est <- colMeans(etas[N/2:N,]) 
+  
+  #computing sample variances
+  var_est <- apply(etas[N/2:N,],2,var)
+  
+  #Computing effective sample sizes
+  #eff_size <- apply(etas[N/2:N,],2,effectiveSize)
+  print(effectiveSize(etas[N/2:N,]))
+  
+  return(est)
+}
+
+mc.postmean.etas <- mc.estimate.smooth(10000)
+
 
